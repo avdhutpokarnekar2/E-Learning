850a2a58a34b4cae7e973a6697ef5a9906278b00
Avdhut Pokarnekar <apokarnekar@gmail.com>
2023-04-19 14:22:04 +0530

    initial setup

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,100 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../stylesheets .css
+// = link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link js/custom.js
+//= link js/jquery-3.2.1.min.js
+//= link styles/bootstrap4/bootstrap.min.css
+//= link styles/bootstrap4/bootstrap.min.js
+//= link styles/bootstrap4/popper.js
+//= link plugins/greensock/TweenMax.min.js
+//= link plugins/greensock/TimelineMax.min.js
+//= link plugins/greensock/animation.gsap.min.js
+//= link plugins/greensock/ScrollToPlugin.min.js
+//= link plugins/video-js/video-js.css
+//= link plugins/OwlCarousel2-2.2.1/owl.carousel.css
+//= link plugins/OwlCarousel2-2.2.1/animate.css
+//= link plugins/scrollmagic/ScrollMagic.min.js
+//= link plugins/OwlCarousel2-2.2.1/owl.carousel.js
+//= link plugins/easing/easing.js
+//= link plugins/video-js/video.min.js
+//= link plugins/video-js/Youtube.min.js
+//= link plugins/parallax-js-master/parallax.min.js
+ //= link js/jquery-3.2.1.min.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
